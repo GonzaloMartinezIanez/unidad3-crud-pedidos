3629e2aec33758f4e71f96d19746475d4911512d
GonzaloMartinezIanez <gonzalomartianez@gmail.com>
2025-12-05 13:37:58 +0100

    Se ha modificado la estructura de datos de las ordenas para que también sea una lista enlazada

--- estructuras_datos.py
+++ estructuras_datos.py
@@ -34,14 +34,14 @@ def search(root : Product, id_product):
         else:
             return search(root.right, id_product)
 
-class Product_list:
+class ProductList:
     def __init__(self, product):
         self.product = product
         self.next = None
 
     def insert(self, product):
         if self.next is None:
-            self.next = Product_list(product)
+            self.next = ProductList(product)
         else:
             self.next.insert(product)
 
@@ -76,37 +76,85 @@ class Product_list:
         else:
             return this_product + self.next.to_string()
 
-class Order:
+class OrderList:
     def __init__(self, id_order):
         self.id_order = id_order
         self.orders = None
+        self.next = None
 
-    def insert(self, new_product):
-        # La lista esta vacia
-        if self.orders is None:
-            self.orders = Product_list(new_product)
-        # Delegar la inserccion al primero de la lista
+    def insert_product(self, id_order, new_product):
+        if self.id_order == id_order:
+            # La lista esta vacia
+            if self.orders is None:
+                self.orders = ProductList(new_product)
+            # Delegar la inserccion al primero de la lista
+            else:
+                self.orders.insert(new_product)
+        elif self.next is not None:
+            self.next.insert_product(id_order, new_product)
+        # Si no hay siguiente, no existe una orden con este id
+
+    def insert_order(self, id_order):
+        if self.next is None:
+              self.next = OrderList(id_order)
         else:
-            self.orders.insert(new_product)
+            self.next.insert_order(id_order)
 
-    def delete(self, id_product):
-        if self.orders is not None:
+    def delete_product(self, id_order, id_product):
+        if self.id_order == id_order:
             self.orders = self.orders.delete(id_product)
+        elif self.next is not None:
+            self.next.delete_product(id_order, id_product)
+        
+    def delete_order(self, id_order):
+        # Hay que borrar el primero
+        if self.id_order == id_order:
+            return self.next
+        
+        aux = self
+        found = False
+        while not found:
+            # No se ha encontrado
+            if aux.next is None:
+                found = True
+            else:
+                # Hay que eliminar el siguiente producto
+                if aux.next.id_order == id_order:
+                    aux.next = aux.next.next
+                    found = True
 
-    def list_to_string(self):
-        return self.orders.to_string()
+            aux = aux.next
+
+        # Se devuelve el primer producto de la lista
+        return self
+
+    def list_to_string(self, id_order):
+        if id_order == self.id_order:
+            return self.orders.to_string()
+        elif self.next is not None:
+            return self.next.list_to_string(id_order)
+
+
+
+
 
 products = Product(24, "Tornillo", "descripcion tornillo", 6)
 products.insert(10, "Tuerca", "descripcion tuerca", 5)
 products.insert(43, "Destornillador", "descripcion destronillador", 49)
 products.insert(20, "LLave inglesa", "descripcion llave inglesa", 123)
 
-print(search(products, 10).description)
+#print(search(products, 10).description)
 
-order_1 = Order(1)
-order_1.insert(search(products, 43))
-order_1.insert(search(products, 10))
-order_1.insert(search(products, 20))
-print(order_1.list_to_string())
-order_1.delete(20)
-print(order_1.list_to_string())
+orders = OrderList(1)
+orders.insert_product(1, search(products, 10))
+orders.insert_product(1, search(products, 43))
+#print(orders.list_to_string(1))
+orders.delete_product(1, 10)
+#print(orders.list_to_string(1))
+orders.insert_order(2)
+orders.insert_product(2, search(products, 24))
+orders.insert_product(2, search(products, 20))
+orders.delete_product(2, 24)
+#print(orders.list_to_string(2))
+orders = orders.delete_order(2)
+print(orders.list_to_string(2))
